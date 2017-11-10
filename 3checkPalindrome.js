63b80b5aa08a258bd936a490217fc103ec0b9a73
Jeovanny <jeov94@gmail.com>
2017-11-10 13:43:39 -0600

    reduce code line

--- 3checkPalindrome.js
+++ 3checkPalindrome.js
@@ -1,15 +1,12 @@
 function checkPalindrome (inputString) {
-     var cont = 0
      var flag = null
 
-     for ( var i = inputString.length ; i > 0 ; i -- ) {
+     for ( var i = inputString.length, cont = 0 ; i > 0 ; i --, cont ++) {
         if ( inputString[cont] === inputString[i - 1] ) {
             flag = true
         } else {
-            flag = false
-            break
+            return false
         }
-          cont++
      }
      return flag
 }
