88a55bcd3ba04701f802e3cfc722b68341826e55
Jeovanny <jeov94@gmail.com>
2017-11-10 14:21:45 -0600

    Add Math.min & Math-min, reduce code lines

--- 6arrayConsecutive.js
+++ 6arrayConsecutive.js
@@ -1,26 +1,7 @@
 function makeArrayConsecutive2(statues) {
-    var min = statues[0]
-    var max = 0
-    var counter = 0
+  var min = Math.min(...statues) //obtiene valor mínimo del array
+  var max = Math.max(...statues) //obtiene valor máximo del array
+  var counter = max - min
 
-
-    for ( var i = 0 ; i < statues.length; i++ ) {
-        if (max < statues[i])
-            {
-                max = statues[i]
-            }
-        if ( min < statues[i]) {
-
-        } else {
-            min = statues[i]
-        }
-    }
-
-for (var i = min ; i <= max ; i++) {
-    counter ++
-}
-
-    var solution = counter - statues.length
-    return solution
-
+  return counter+1 - statues.length
 }
