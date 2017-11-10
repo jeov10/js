e080c604133fec0f72e4ff72d53ec7a60bd119a7
Jeovanny <jeov94@gmail.com>
2017-11-10 15:08:50 -0600

    delete unnecessary variables

--- 8matrixElementSum.js
+++ 8matrixElementSum.js
@@ -1,24 +1,21 @@
 function matrixElementsSum(matrix) {
+    var solve = 0
 
-var solve = 0
-var column = matrix[0].length;
-var row =  matrix.length;
-
-for (var i = 0 ; i < row ; i++) {
-  for (var j = 0 ; j < column ; j++) {
-      if (matrix[i][j]===0) { //busca los ceros
-          for (var a = i ; a < row ; a++) {
-              matrix[a][j] = 0 // elimina todos los datos debajo de 0's
+    for (var i = 0 ; i < matrix.length ; i++) {
+      for (var j = 0 ; j < matrix[0].length ; j++) {
+          if (matrix[i][j]===0) { //busca los ceros
+              for (var a = i ; a < matrix.length ; a++) {
+                  matrix[a][j] = 0 // elimina todos los datos debajo de 0's
+              }
           }
       }
-  }
-}
+    }
 
-for (var i = 0 ; i < row ; i++) { //suma todos los elementos
-  for (var j = 0 ; j < column ; j++) {
-    solve = solve + matrix[i][j]
- }
-}
+    for (var i = 0 ; i < matrix.length ; i++) { //suma todos los elementos
+      for (var j = 0 ; j < matrix[0].length ; j++) {
+        solve = solve + matrix[i][j]
+     }
+    }
 
-return solve
-}
+    return solve
+    }
