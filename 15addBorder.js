0efafa25315106f27db0c6eb0b61cb0bc99e70b9
Jeovanny <jeov94@gmail.com>
2017-11-10 18:58:26 -0600

    solved

---
+++ 15addBorder.js
@@ -0,0 +1,19 @@
+function addBorder(picture) {
+  var aux = "", firstRow = ""
+  var pictureLength = picture.length
+  var cont = 0
+
+  while(cont < picture[0].length+2){
+    firstRow = firstRow + "*"
+    cont++
+  }
+
+  for (var i = pictureLength ; i > 0 ; i --) {
+    aux = "*" + picture[i-1] + "*"
+    picture[i] = aux
+  }
+
+  picture[0] = firstRow
+  picture[pictureLength+1] = firstRow
+  return picture
+}
