e984ea1956b4c89faf5a5ec321f820d64a47e77f
Jeovanny <jeov94@gmail.com>
2017-11-13 12:18:16 -0600

    reinterpreting the problem

--- 16areSimilar.js
+++ 16areSimilar.js
@@ -1,20 +1,42 @@
+function areSimilar(a, b) {
+  var cont = 0
+  var solution = null
+  var aux = 0
+
+  while(cont < a.length){
+    for (var i = 0 ; i < a.length ; i ++){
+      if (a[i] === b[i]) {
+        a.splice(i, 1)
+        b.splice(i, 1)
+      }
+    }
+    cont ++
+  }
+
+  if (a.length === 0) {
+    return true
+  }
+
+  aux = b[0]
+  b[0] = b[1]
+  b[1] = aux
+
+  if (a[0] === b[0]) {
+    a.splice(0, 1)
+    b.splice(0, 1)
+  } else {
+    return false
+  }
+
+  if(a.length === 0) {
+    return true
+  } else {
+    return false
+  }
+
+}
+
 var a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279]
 var b = [832, 998, 148, 570, 533, 561, 455, 147, 894, 279]
-var cont = 0
-var solution = null
 
-do {
-for(i=0 ; i < a.length ; i++){
-  for(j=0 ; j < b.length ; j++){
-    if(a[i] === b[j]) {
-        a.splice(i, 1)
-        b.splice(j, 1)
-        i = j = 0
-    }
-    console.log()
-  }
-}
-cont ++
-}while(cont <= a.length)
-
-a.length === 0 ? solution = true : solution = false
+areSimilar(a,b)
