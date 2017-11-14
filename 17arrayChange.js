29cdfb9a2f105c3347dd0160ea548ea26dc31f78
jeov10 <32721103+jeov10@users.noreply.github.com>
2017-11-14 14:28:28 -0600

    17 Solved and Make a function

--- 17arrayChange.js
+++ 17arrayChange.js
@@ -1,9 +1,11 @@
-var inputArray = [-1000, 0, -2, 0]
-var loopCounter = 0
-
-for (var i = 0 ; i < inputArray.length ; i++ ) {
-  while (inputArray[i] >= inputArray[i+1]) {
-    inputArray[i+1] ++
-    loopCounter ++
+function arrayChange(inputArray) {
+  for (var i = 0, loopCounter = 0 ; i < inputArray.length ; i ++ ) {
+    while (inputArray[i] >= inputArray[i + 1]) {
+      inputArray[i + 1] ++ //incrementa el valor posterior hasta que sea mayor que el anterior
+      loopCounter ++ //guarda el número de ciclos para lograr una cadena creciente
+    }
   }
+  return loopCounter
 }
+//var inputArray = [-1000, 0, -2, 0]
+//arrayChange(inputArray)
