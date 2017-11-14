5042a4379509ada576185c92e350a71cc75e3c73
Jeovanny <jeov94@gmail.com>
2017-11-14 12:48:31 -0600

    Problem Solved

--- 16areSimilar.js
+++ 16areSimilar.js
@@ -2,15 +2,24 @@ function areSimilar(a, b) { // el array es exactamente igual
   if(a.toString() === b.toString()) {
       return true
   }
-  var c = []
-  var d = []
+  var aResidue = []
+  var bResidue = []
 
   for(var i = 0 ; i < a.length ; i ++) { //compara elemento vs elemento
     if(a[i] != b[i]) { //saca los elementos que no coinciden
-    c.push(a[i])
-    d.push(b[i])
+      aResidue.push(a[i])
+      bResidue.push(b[i])
     }
   }
 
-var a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279]
-var b = [832, 998, 148, 570, 533, 561, 455, 147, 894, 279]
+  if(aResidue.length <= 2) { // Los elementos a cambiar es menor a dos
+    bResidue = bResidue.reverse() //cambio de posiciones del array 2
+    if(aResidue.toString() === bResidue.toString()){ //compara ambos residuos como cadenas
+      return true
+    } else {
+      return false
+    }
+  } else {
+    return false
+  }
+}
