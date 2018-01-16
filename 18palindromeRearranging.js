855663518f87b52a05482b51f5466404959d1532
jeov10 <32721103+jeov10@users.noreply.github.com>
2018-01-16 17:30:06 -0600

    no working yet

--- 18palindromeRearranging.js
+++ 18palindromeRearranging.js
@@ -1,24 +1,33 @@
-var inputString = "bbabba"
-inputString = Array.from(inputString)
-var reverseString = inputString.slice().reverse()
-var cont = 0
+var inputString = "abbcde"
 
-//if(inputString.toString() === reverseString.toString()) { //si la cadena es palindromo sin modificar
-//    return true
-//}
+if(inputString === inputString.split("").reverse().join("")){
+  //return true
+  console.log('true')
+} //comprueba si es palindromo
+
+inputString = Array.from(inputString) //convierte la cadena a array
 
 for (var i = 0 ; i < inputString.length ; i ++) {
-  for (var j = 1 ; j < sizeString ; j ++) {
+  for (var j = 1 ; j < inputString.length ; j ++) {
     if(inputString[i] === inputString[j]) {
-      inputString.splice(j, 1)
-      inputString.splice(i, 1)
-      i = 0
+      console.log(inputString[i] + ' coincide con ' + inputString[j])
+      console.log(i + ' pos ' + j)
+      inputString.splice(j, 1)//Borra del array el elem seleccionado
+      inputString.splice(i, 1)//Borra del array el elem seleccionado
+      j = 1;
     }
     console.log(inputString)
   }
-  console.log('i: ' + i + ' j: ' + j)
+  i = 0;
 }
 
+if(inputString.length < 2) {
+  //return true
+  console.log('true')
+} else {
+  //return false
+  console.log('false')
+}
 
 //si el palíndromo es par el inputString quedará vacío
 //si el palíndromo es impar tendrá sólo un elemento
