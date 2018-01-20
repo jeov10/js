ea9c79ec3fc933dd1e3bc557bf3e36dbbe2961c4
jeov10 <32721103+jeov10@users.noreply.github.com>
2018-01-19 18:25:39 -0600

    almos ready

--- 18palindromeRearranging.js
+++ 18palindromeRearranging.js
@@ -1,34 +1,22 @@
-var inputString = "abbcde"
+inputString = "abbcde"
 
 if(inputString === inputString.split("").reverse().join("")){
   //return true
   console.log('true')
 } //comprueba si es palindromo
 
-inputString = Array.from(inputString) //convierte la cadena a array
-
-for (var i = 0 ; i < inputString.length ; i ++) {
-  for (var j = 1 ; j < inputString.length ; j ++) {
-    if(inputString[i] === inputString[j]) {
-      console.log(inputString[i] + ' coincide con ' + inputString[j])
-      console.log(i + ' pos ' + j)
-      inputString.splice(j, 1)//Borra del array el elem seleccionado
-      inputString.splice(i, 1)//Borra del array el elem seleccionado
-      j = 1;
-    }
-    console.log(inputString)
-  }
-  i = 0;
+letterCount = { }
+for (i = 0; i < inputString.length; i++) { //crea una objeto e inicializa cada letra (unico) en cero
+  letter = inputString[i]
+  console.log('letter: ' + letter)
+  letterCount[letter] = 0
+  console.log(letterCount)
+}
+for (var i = 0; i < inputString.length; i++) { //cuentas cuantas veces aparece una letra
+  letter = inputString[i]
+  letterCount[letter]++
+  console.log(letterCount)
 }
 
-if(inputString.length < 2) {
-  //return true
-  console.log('true')
-} else {
-  //return false
-  console.log('false')
-}
 
-//si el palíndromo es par el inputString quedará vacío
-//si el palíndromo es impar tendrá sólo un elemento
-//si el inputString resultante tiene 2 o más elementos no es in palíndromo
+console.log(letterCount)
