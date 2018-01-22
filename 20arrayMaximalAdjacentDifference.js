828988c74376d053b61b7ab482248811689ecaa7
jeov10 <32721103+jeov10@users.noreply.github.com>
2018-01-22 15:34:31 -0600

    Fixed and finished

--- 20arrayMaximalAdjacentDifference.js
+++ 20arrayMaximalAdjacentDifference.js
@@ -1,12 +1,10 @@
-inputArray = [2, 4, 1, 0]
+function arrayMaximalAdjacentDifference(inputArray) {
+  maxDifference = 0
 
-maxDifference = 0
-
-for (i = 0; i < inputArray.length-1; i ++) {
-  aux = Math.abs(inputArray[i] - inputArray[i + 1]) //Diferencia absoluta
-  if(aux > maxDifference) {
-    maxDifference = aux
+  for (i = 0; i < inputArray.length-1; i ++) {
+    aux = Math.abs(inputArray[i] - inputArray[i + 1]) //Diferencia absoluta
+    maxDifference = Math.max(maxDifference, aux) //guarda el mayor de ambos
   }
-}
 
-console.log(maxDifference);
+  return maxDifference
+}
