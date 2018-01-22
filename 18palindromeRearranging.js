fa75b1e3e3873496af8a72d25c7884441d8caffb
jeov10 <32721103+jeov10@users.noreply.github.com>
2018-01-22 12:09:45 -0600

    pre-function

--- 18palindromeRearranging.js
+++ 18palindromeRearranging.js
@@ -1,31 +1,21 @@
-inputString = "abbbba"
+inputString = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaabc"
 
 if(inputString === inputString.split("").reverse().join("")){
   //return true
   console.log('true')
 } //comprueba si es palindromo
 
-letterCount = { }
-letterSum = 0
+letterCount = {}
+
 for (i = 0; i < inputString.length; i++) { //crea una objeto e inicializa cada letra (unico) en cero
   letter = inputString[i]
-  letterCount[letter] = 0
-}
-
-for (var i = 0; i < inputString.length; i++) { //cuentas cuantas veces aparece una letra
-  letter = inputString[i]
+  letterCount[letter] = letterCount[letter] || 0 //
   letterCount[letter]++
 }
 
-for (var i = 0; i < inputString.length; i++) { //Determina si es palindromo
-  letter = inputString[i]
-  letterSum += letterCount[letter] % 2
+const arr = []
+for (var key in letterCount) {
+  (key % 2 != 0) && arr.push(key)
 }
 
-if (letterSum < 2) {
-  //return true
-  console.log('true')
-} else {
-  //return false
-  console.log('false')
-}
+arr.length > 1 ? console.log(false) : console.log(true)
